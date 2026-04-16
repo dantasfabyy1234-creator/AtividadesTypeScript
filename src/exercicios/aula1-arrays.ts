839c6b893b0a5216baddb73328b87595d6bf890c
josevitor21122-byte <josevitor21122@gmail.com>
2026-04-16 14:30:11 -0300

    Aula 1- arrays

---
+++ src/exercicios/aula1-arrays.ts
@@ -0,0 +1,11 @@
+let vetor:(number|string)[] = [10,200,"maria","jose"]
+
+vetor.push(30) //adcionar elemento no final do vetor
+vetor.pop() // remove elementos no final do vetor, ,ou seja, o ultimo elemento do vetor
+vetor.unshift("roxo") // é o oposto de push, ele adciona no inicio do vetor
+vetor.shift() // é o oposto do pop, ele remove o primeiro elemento
+
+
+vetor.reverse() //inverte o vetor
+vetor.sort() // ordena em ordem alfabetica e crescente
+console.log(vetor)
